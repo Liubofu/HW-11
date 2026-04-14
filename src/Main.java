0a8bae2d96f1df240ad41dcdfc6525a8e78959df
Liubofu <luboff1992@gmail.com>
2026-04-14 20:08:35 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public static void CounterYears(int year) {
+            if (year > 1584) {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                    System.out.println(year + " год является високосным");
+                } else {
+                    System.out.println(year + " год не является високосным");
+                }
+            }
         }
+
+        public static void main(String[] args) {
+            CounterYears(1992);
+        }
+
+    int currentYear = LocalDate.now().getYear();
+        int
+    int clientOS = 1;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+        System.out.println("«Установите облегченную версию приложения для Android по ссылке»");
+    } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+        System.out.println("«Установите обычную версию приложения для Android по ссылке»");
     }
-}
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+        System.out.println("«Установите облегченную версию приложения для iOS по ссылке»");
+    } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+        System.out.println("«Установите обычную версию приложения для iOS по ссылке»");
+    }
+    }
+
